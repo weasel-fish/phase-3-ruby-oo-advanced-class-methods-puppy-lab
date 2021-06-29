4dba5f22d2bf51ec7c5e790a319099f0e4902ae3
github username <github email address>
2021-06-29 15:45:34 -0400

    commit tree db701f2d981996d4d1d8cb4b521b31347be2a680

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+class Dog
+    @@all = []
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+end
